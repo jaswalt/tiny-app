7bb19cff30549489140278a464c07b313ac3b56a
Jasper Walton <jasper.lyn.walton@gmail.com>
2017-10-11 17:30:58 +0000

    handles post for a new url submission

--- express_server.js
+++ express_server.js
@@ -2,12 +2,25 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca"},
   "9sm5xK": {longURL: "http://www.google.com"}
 };
+
+function generateRandomString(length) {
+  let randomString = "";
+  const charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++) {
+    randomString += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return randomString;
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -18,6 +31,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  if (!req.body.longURL) {
+    res.status(400).send("Please input a URL.")
+  } else {
+    const shortURL = generateRandomString(6);
+    urlDatabase[shortURL] = { longURL: req.body.longURL }
+    res.redirect("/urls/" + shortURL);
+  }
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase };
   res.render("urls_show", templateVars);
@@ -26,3 +53,21 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
